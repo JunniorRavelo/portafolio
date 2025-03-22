8e68bf9d7d66048f2647dac5f2118d97919d8d94
Junnior Ravelo <jr9760267@gmail.com>
2025-03-22 09:50:04 -0500

    se cambia el idioma de la aplicación de inglés a español en el archivo layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="es" suppressHydrationWarning>
       <head />
       {/* Google Analytics Scripts */}
       <Script
